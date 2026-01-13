0e7537ed076d56d46cb60c1f3cf985c45890d010
Aadi-420 <adarshashodiya420@gmail.com>
2026-01-13 16:07:19 +0530

    Issue fix 1

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,38 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
+    browsers: ['ChromeHeadlessCI'],
+
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    },
+
     reporters: ['progress', 'coverage'],
 
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
-      subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'lcov'},
+        { type: 'lcovonly' },
+        { type: 'text-summary' }
       ]
-    }
+    },
+
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
